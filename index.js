b4d152b12315ff3b09f37ec6a773b07fb7e04d33
sooshe <sooshe@sooshebot.com>
2020-02-10 13:31:24 -0600

    add port and server.listen(), npm run server

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const db = require("./data/db");
 const server = express();
 
 server.use(express.json());
+
+const port = 5000;
+
+server.listen(port, () => console.log(`\n** API on port ${port} \n`));
