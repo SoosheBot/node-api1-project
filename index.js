0a91a0dbded8c5c5e04938efb3849e32f880bb62
sooshe <sooshe@sooshebot.com>
2020-02-10 13:35:44 -0600

    write GET request, successful test in postman

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // implement your API here
 const express = require('express');
-
 const db = require("./data/db");
 
 const server = express();
-
 server.use(express.json());
 
-const port = 5000;
+server.get('/api/users', (req,res) => {
+    db.find()
+    .then(user => {
+        console.log('users found', user);
+        res.status(201).json(user);
+    })
+    .catch(err => {
+        console.log('GET error', err);
+        res.status(500).json({ errorMessage: 'ope!'})
+    });
+});
 
+const port = 5000;
 server.listen(port, () => console.log(`\n** API on port ${port} \n`));
