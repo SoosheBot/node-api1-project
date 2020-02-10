280f10d0787c9122074e37efcc29a0a2272dcd0a
sooshe <sooshe@sooshebot.com>
2020-02-10 13:52:32 -0600

    debug post request

--- index.js
+++ index.js
@@ -5,17 +5,33 @@ const db = require("./data/db");
 const server = express();
 server.use(express.json());
 
+//GET
 server.get('/api/users', (req,res) => {
     db.find()
-    .then(user => {
-        console.log('users found', user);
-        res.status(201).json(user);
+    .then(users => {
+        console.log('users found', users);
+        res.status(201).json(users);
     })
     .catch(err => {
         console.log('GET error', err);
         res.status(500).json({ errorMessage: 'ope!'})
     });
 });
+
+//POST
+server.post('/api/users', (req,res) => {
+    const dbInfo = req.body;
+    console.log("body", req.body);
+
+    db.insert(dbInfo).then(user => {
+        console.log("user is", user);
+        res.status(201).json(user);
+    })
+    .catch(err => {
+        console.log('GET error', err);
+        res.status(400).json({ errorMessage: 'ope! Add the username and bio'})
+    });
+})
 
 const port = 5000;
 server.listen(port, () => console.log(`\n** API on port ${port} \n`));
