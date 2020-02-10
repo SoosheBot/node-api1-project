c3429e993279f5e7fea153f37df729ee01cd818d
sooshe <sooshe@sooshebot.com>
2020-02-10 15:18:40 -0600

    get by id success

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ server.use(express.json());
 
 //POST
 server.post("/api/users", (req, res) => {
-  const users = { ...req.body};
+  const users = { ...req.body };
 
   db.insert(users)
     .then(user => {
@@ -16,41 +16,63 @@ server.post("/api/users", (req, res) => {
         res.status(201).json(user);
       } else {
         // console.log("POST error", err);
-        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" });
+        res
+          .status(500)
+          .json({
+            errorMessage:
+              "There was an error while saving the user to the database"
+          });
       }
     })
     .catch(err => {
-        console.log("POST error--missing user name or bio", err);
-        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+      console.log("POST error--missing user name or bio", err);
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." });
     });
 });
 
 //GET
 server.get("/api/users", (req, res) => {
-    db.find()
-      .then(users => {
-        console.log("GET success! Users found", users);
-        res.status(201).json(users);
-      })
-      .catch(err => {
-        console.log("GET error", err);
-        res.status(500).json({ errorMessage: "The users information could not be retrieved." });
-      });
-  });
+  db.find()
+    .then(users => {
+      console.log("GET success! Users found", users);
+      res.status(201).json(users);
+    })
+    .catch(err => {
+      console.log("GET error", err);
+      res
+        .status(500)
+        .json({
+          errorMessage: "The users information could not be retrieved."
+        });
+    });
+});
 
 //GET by id
 server.get("/api/users/:id", (req, res) => {
-    const { id } = req.params;
-    db.findById(id)
-      .then(users => {
-        console.log("GET success! Users found", users);
+  const { id } = req.params;
+  db.findById(id)
+    .then(users => {
+      if (users) {
+        console.log("GET by id success! User found", users);
         res.status(201).json(users);
-      })
-      .catch(err => {
+      } else {
         console.log("GET error", err);
-        res.status(500).json({ errorMessage: "ope!" });
-      });
-  });
+        res
+          .status(500)
+          .json({
+            errorMessage: "The user's information could not be retrieved."
+          });
+      }
+    })
+    .catch(err => {
+      console.log("GET by ID error", err);
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+    });
+});
 
 const port = 5000;
 server.listen(port, () => console.log(`\n** API on port ${port} \n`));
